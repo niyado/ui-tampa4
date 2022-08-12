c79ccc728114fc81fecf8ac31b99f39ac6ef66c5
Niya Donova <niyadonova@gmail.com>
2022-08-12 14:11:19 -0400

    bug fix

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from git import safe_decode
 import streamlit as st
 import pandas as pd
 import requests
@@ -17,28 +18,31 @@ def main():
     session = requests.Session()
 
     apiKey = "24QB9PEXI24YZT6M"
+    safe_proceed = True
     
     sp500 = pd.read_csv("datasets/SP500.csv")
-    symbols = sp500['Symbol'].sort_values().tolist()  
+    symbols = sp500['Symbol'].sort_values().tolist()
+    symbols.insert(0, "SELECT")  
     
     #create portfolio list here, using a getAll endpoint
-    portfolio = ["SELECT", "IBM", "AMZN", "AAPL"]
+    portfolio = ["IBM", "AMZN", "AAPL"]
+    portfolio.insert(0, "SELECT")
 
     tab1, tab2, tab3 = st.tabs(["Stocks", "ETFs", "Mutual Funds"])
 
     with tab1:
         st.header("Stocks")
         with st.sidebar:
-            with st.form("my_form"):
+            with st.form("my_form", clear_on_submit=True):
                 #symbol = st.number_input("Symbol", key="symbol")      
 
-                ticker = st.selectbox(
+                ticker1 = st.selectbox(
                     'Choose a S&P 500 Stock',
                     symbols)
 
                 st.text("\n\nOR\n\n")
 
-                ticker = st.selectbox(
+                ticker2 = st.selectbox(
                     'Choose from your portfolio',
                     portfolio)
 
@@ -46,11 +50,21 @@ def main():
 
         if submitted:
             st.write("Result")
-            data = fetch(session, f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={apiKey}")
-            if data:
-                st.text(data)
+            if(ticker2 == "SELECT"):
+                if(ticker1 != "SELECT"):
+                    ticker = ticker1
+                else:
+                    st.error("Please select a ticker before proceeding")
+                    safe_proceed = False
             else:
-                st.error("Error")
+                ticker = ticker2
+
+            if safe_proceed:
+                data = fetch(session, f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={ticker}&apikey={apiKey}")
+                if data:
+                    st.text(data)
+                else:
+                    st.error("Error")
 
     with tab2:
         st.header("ETFs")
