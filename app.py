481f78bcc2204fc33cb2d1bca9cb57d948f9523b
Niya Donova <niyadonova@gmail.com>
2022-08-12 16:11:12 -0400

    dummy json successfully processed into usable data

--- app.py
+++ app.py
@@ -26,17 +26,9 @@ def main():
     
     #create portfolio list here, using a getAll endpoint, right now we only have a placeholder
     portfolio = ["IBM", "AMZN", "AAPL"]
-    dummyDataJSON = {"Stocks": [
-    {"symbol": "AAPL", "averageCostPerShare": 192.99, "quantity": 23}, 
-    {"symbol": "IBM", "averageCostPerShare": 92.99, "quantity": 6}, 
-    {"symbol": "A", "averageCostPerShare": 150.32, "quantity": 10}, 
-    {"symbol": "AMC", "averageCostPerShare": 34.45, "quantity": 36}, 
-    {"symbol": "TJX", "averageCostPerShare": 85.34, "quantity": 5}, 
-    {"symbol": "AMZN", "averageCostPerShare": 292.99, "quantity": 18}, 
-    {"symbol": "HCA", "averageCostPerShare": 92.59, "quantity": 24}
-    ]}
+    dummyDataJSON = '{"Stocks": [{"symbol": "AAPL", "averageCostPerShare": 192.99, "quantity": 23}, {"symbol": "IBM", "averageCostPerShare": 92.99, "quantity": 6}, {"symbol": "A", "averageCostPerShare": 150.32, "quantity": 10}, {"symbol": "AMC", "averageCostPerShare": 34.45, "quantity": 36}, {"symbol": "TJX", "averageCostPerShare": 85.34, "quantity": 5}, {"symbol": "AMZN", "averageCostPerShare": 292.99, "quantity": 18}, {"symbol": "HCA", "averageCostPerShare": 92.59, "quantity": 24}]}'
 
-    dummyData = json.dumps(dummyDataJSON)
+    dummyData = json.loads(dummyDataJSON)
 
 
     tab1, tab2 = st.tabs(["Portfolio", "Trade"])
@@ -44,7 +36,15 @@ def main():
     with tab1:
         st.header("Portfolio")
         st.text("Show portfolio stats here")
-        st.text(dummyData)
+        #st.text(dummyData)
+        tickersProcessed = []
+        allTickers = dummyData["Stocks"]
+        for s in allTickers:
+            m = (s['symbol'], s['averageCostPerShare'], s['quantity'])
+            tickersProcessed.append(m)
+        for s in tickersProcessed:
+            st.text(s)
+
 
     with tab2:
         st.header("Trade")
