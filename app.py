e40e26995467a09babf14dee69dde95645faa726
Niya Donova <niyadonova@gmail.com>
2022-08-12 16:30:52 -0400

    added portfolio diversity chart

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from turtle import width
 import streamlit as st
 import pandas as pd
 import requests
 import json
+import plotly.graph_objects as go
 
 
 def fetch(session, url):
@@ -19,13 +19,11 @@ def main():
     session = requests.Session()
 
     apiKey = "24QB9PEXI24YZT6M"
-    safe_proceed = True
     
     sp500 = pd.read_csv("datasets/SP500.csv")
     symbols = sp500['Symbol'].sort_values().tolist()
     
     #create portfolio list here, using a getAll endpoint, right now we only have a placeholder
-    portfolio = ["IBM", "AMZN", "AAPL"]
     dummyDataJSON = '{"Stocks": [{"symbol": "AAPL", "averageCostPerShare": 192.99, "quantity": 23}, {"symbol": "IBM", "averageCostPerShare": 92.99, "quantity": 6}, {"symbol": "A", "averageCostPerShare": 150.32, "quantity": 10}, {"symbol": "AMC", "averageCostPerShare": 34.45, "quantity": 36}, {"symbol": "TJX", "averageCostPerShare": 85.34, "quantity": 5}, {"symbol": "AMZN", "averageCostPerShare": 292.99, "quantity": 18}, {"symbol": "HCA", "averageCostPerShare": 92.59, "quantity": 24}]}'
 
     dummyData = json.loads(dummyDataJSON)
@@ -35,15 +33,29 @@ def main():
 
     with tab1:
         st.header("Portfolio")
-        st.text("Show portfolio stats here")
-        #st.text(dummyData)
-        tickersProcessed = []
+        
+        tickersProcessed = [[],[],[]]
         allTickers = dummyData["Stocks"]
         for s in allTickers:
-            m = (s['symbol'], s['averageCostPerShare'], s['quantity'])
-            tickersProcessed.append(m)
-        for s in tickersProcessed:
-            st.text(s)
+            sym = s['symbol']
+            cost = s['averageCostPerShare']
+            quantity = s['quantity']
+            tickersProcessed[0].append(sym)
+            tickersProcessed[1].append(cost)
+            tickersProcessed[2].append(quantity)
+
+        #diversity plot
+        fig = go.Figure(
+            go.Pie(
+            labels = tickersProcessed[0],
+            values = tickersProcessed[2],
+            hoverinfo = "label+percent",
+            textinfo = "value"
+        ))
+
+        st.subheader("Diversity")
+        st.plotly_chart(fig)
+        
 
 
     with tab2:
