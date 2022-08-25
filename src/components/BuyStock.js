b66f0cd9e17b10355dbe970c38715a8fbe3911b9
Sebodroid <barnessebas99@gmail.com>
2022-08-24 22:07:37 -0400

    fixed buy and sell functionality

--- src/components/BuyStock.js
+++ src/components/BuyStock.js
@@ -5,8 +5,38 @@ import "../App.css"
 const BuyStock = (props) => {
 
     const [symbol, setSymbol] = useState([]);
+    const [stockName, setStockName] = useState([]);
     const [sharesAmount, setSharesAmount] = useState([]);
-    console.log("Hello")
+    const [purchasePrice, setPurchasePrice] = useState([]);
+
+    const buyJSON = {
+        "symbol": symbol,
+        "name": stockName,
+        "shares": sharesAmount,
+        "price": purchasePrice,
+        "timestampOfTrade": 123243424,
+        "type": "BUY",
+        "securityType": "stock"
+    }
+
+    const setStockPurchase = (e) => {
+        setSharesAmount(e.target.value)
+        setPurchasePrice(e.target.value * props.price)
+        console.log(e.target.value)
+        setSymbol(props.symbol)
+        setStockName(props.name)
+    }
+
+    const purchaseFunc = () => {
+        fetch("http://demospringdatabase-demospringdatabase.openshift20.conygre.com/api/portfolio/trades/newTrade", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': "*"
+            },
+            body: JSON.stringify(buyJSON)
+        })
+    }
 
     return (
         <div className='stockContainer'>
@@ -21,12 +51,13 @@ const BuyStock = (props) => {
                                 shrink: true,
                             }}
                             inputProps={{ inputMode: 'numeric', min: 0, }}
-                            onChange={(e) => setSharesAmount(e.target.value)}
+                            onChange={(e) => setStockPurchase(e)}
                         />
-                        <label>Total Price {props.price * sharesAmount}</label>
+                        <label>Total Price {purchasePrice}</label>
                     </FormControl>
                 </CardContent>
             </Card>
+            <Button variant="contained" onClick={purchaseFunc}>Purchase</Button>
             <Button variant='contained' onClick={props.func}>Cancel</Button>
         </div>
     )
